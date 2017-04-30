228cea1984e87a7a0add9f8a37aa4b4f56fcc3e9
Mikhail Ziankevich <zin@zame-dev.org>
2017-04-30 13:29:52 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.css';
+
+const App = () => (
+	<div className={styles.app}>
+		<h2>Hello, 123123123213</h2>
+	</div>
+);
+
+export default App;
